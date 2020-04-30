33eee4945eefc446960e4728f0f9ad0350a0f324
宇薪 邱 <simoncat524@hotmail.com>
2020-04-30 15:01:28 +0800

    commit to git

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace GitPractice
             Console.WriteLine($"Hello World!{user.Account}，我是origin/master");
             //狀況二:這是有勾追蹤遠端分支 看有甚麼差異
             Console.WriteLine($"追蹤遠端分支 Checked!!!");
-            //先Update到localmaster
+            //先Update到localmaster //差異應該就只差在後面多一段文字以後都開好了 比較好看
         }
     }
 }
