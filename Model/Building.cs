ae6f10c861c2def2a6ca3aecad5dbb2ad3053fd4
FocacciaSyin <simoncat524@hotmail.com>
2024-01-22 23:26:41 +0800

    [feature] Add Building Model

---
+++ Model/Building.cs
@@ -0,0 +1,11 @@
+﻿using GitPractice.Common;
+
+namespace GitPractice.Model
+{
+    public class Building : BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
