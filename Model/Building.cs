10e0353bf9f894a12a213540c5d180812d48dfee
FocacciaSyin <simoncat524@hotmail.com>
2024-01-25 22:51:17 +0800

    [Reset] to another version

---
+++ Model/Building.cs
@@ -0,0 +1,14 @@
+﻿using GitPractice.Common;
+
+namespace GitPractice.Model
+{
+    public class Building : BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Type { get; set; }
+        public double Pin { get; set; }
+        public int Age { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,8 +19,11 @@ namespace GitPractice
 
             //狀況一:這版是origin/master
             Console.WriteLine($"Hello World!{user.Account}，我是origin/master");
-            //狀況二:這是"沒有"追蹤遠端分支 推到 Git上
-            Console.WriteLine($"xXXXX追蹤遠端分支!!!");
+            //狀況二:這是有勾追蹤遠端分支 看有甚麼差異
+            Console.WriteLine($"追蹤遠端分支 Checked!!!");
+            //先Update到localmaster //差異應該就只差在後面多一段文字以後都開好了 比較好看
+            
+            //2024-01-25 重新送出一次
         }
     }
 }
